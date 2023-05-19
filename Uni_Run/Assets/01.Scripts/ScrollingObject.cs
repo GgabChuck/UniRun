2e29972339a0dcbd19f7e9fd8a569bca346a8b9b
GgabChuck <rlqja3605@gmail.com>
2023-05-19 12:44:28 +0900

    20230519
    P.516

--- Uni_Run/Assets/01.Scripts/ScrollingObject.cs
+++ Uni_Run/Assets/01.Scripts/ScrollingObject.cs
@@ -6,7 +6,10 @@ public class ScrollingObject : MonoBehaviour
     public float speed = 10f; // 이동 속도
     void Update()
     {
+        if (!GameManager.instance.isGameover)
+        { 
         // 초당 speed의 속도로 왼쪽으로 평행이동
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
